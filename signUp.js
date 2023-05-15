e088a07e36b7f7920d49d90bc3be0de7f63be4d5
v <64693573+victorbianconi@users.noreply.github.com>
2023-05-15 20:07:58 +0200

    Update signUp.js

--- signUp.js
+++ signUp.js
@@ -1,12 +1,21 @@
 import sendVerificationCode from "./sendVerificationCode";
 
 const signUp = async (req, res) => { 
+  const client = await clientPromise;
+  const db = client.db("mydatabase");
+  
   const { username, password, email } = req.body;
   
- // the rest of the logic (sanitizing, validating the inputs and creating the user DB document)
+  // the rest of the logic (sanitizing, validating the inputs, hashing the password, etc.)
+  
+  const user = db.users.insertOne({...})
   
   const verificationCode = Math.floor(100000 + Math.random() * 900000);
   await sendVerificationCode({ username, email, code: verificationCode });
+  
+  // example, store the code in the user document so you can later check if it's indeed correct
+  user.verification_code = verificationCode; 
+  await user.save();
 
  // return an access token to the user so he can submit the verification code with it (protected API route) 
 };
